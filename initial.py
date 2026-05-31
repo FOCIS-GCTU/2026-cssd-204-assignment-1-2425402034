4643ae50905d487742b11a866b56ae4e08616123
2425402034 <2425402034@live.gctu.edu.gh>
2026-05-31 21:20:30 +0000

    Add files via upload

---
+++ initial.py
@@ -0,0 +1,26 @@
+def main():
+
+    print("...LLO")
+
+print("...LLLL................LLLL................OOOOOOOOOOOO")
+print("...LLLL................LLLL................OOOOOOOOOOOO")
+print("...LLLL................LLLL................OOO......OOO")
+print("...LLLL................LLLL................OOO......OOO")
+print("...LLLL................LLLL................OOO......OOO")
+print("...LLLL................LLLL................OOO......OOO")
+print("...LLLL................LLLL................OOO......OOO")
+print("...LLLL................LLLL................OOO......OOO")
+print("...LLLLLLLLLLLL...**...LLLLLLLLLLLL...**...OOOOOOOOOOOO")
+print("...LLLLLLLLLLLL...**...LLLLLLLLLLLL...**...OOOOOOOOOOOO")
+
+#File: lord initial.py
+# Description: Print out my initials.
+# Assignment Number: 1
+
+# Name: Lord Listowel Oppong
+# STUDENT ID:2425402034
+# Email: 2425402034@gctu.live.edu.gh
+# Grader: Augustus
+#
+# On my honor,Lord Listowel Oppong , this programming assignment is my own work
+# and I have not provided this code to any other student.
